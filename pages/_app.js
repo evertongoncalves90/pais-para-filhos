d80f343a4abe2b0389777a1b966196be28b5b891
evertongoncalves90 <evertongoncalves90@gmail.com>
2024-09-26 08:49:57 -0300

    Implementação do analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
+// pages/_app.js
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -7,11 +9,16 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 {/* Importando a fonte Poppins */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+                    rel="stylesheet"
+                />
             </Head>
             <Component {...pageProps} />
+            <Analytics />
         </>
     );
 }
+
 export default MyApp;
